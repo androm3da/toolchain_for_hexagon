e5755903ecb83b816b5dcf4a2467c582c0b2eeac
Brian Cain <brian.cain@oss.qualcomm.com>
2026-07-25 10:28:54 -0500

    Dockerfile, bsub-build-in-container.sh: bump ELD to 23.1.0-rc1
    ELD v22.1.0-rc3 does not build against LLVM 23.x -- it calls an undeclared
    'xxHash64', an API drift upstream ELD had not caught up to at that release.
    The 23.1.0-rc1 tag tracks the same LLVM release this branch now builds.
    
    Note the tag has no leading 'v', unlike the previous one; with the patch-tag
    default now derived from ${VER}, patches/eld/<tag> lines up with the archive
    tag name again (there are no eld patches for this release yet, so none are
    applied).
    
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>

--- bsub-build-in-container.sh
+++ bsub-build-in-container.sh
@@ -29,7 +29,7 @@ VER=23.1.0-rc1
 # needed to build RTSan/TySan/Scudo/GWP-ASan for hexagon (see
 # cmake/caches/hexagon-stage0.cmake).
 LLVM_SRC_URL="https://github.com/llvm/llvm-project/archive/refs/tags/llvmorg-${VER}.tar.gz"
-ELD_SRC_URL="https://github.com/qualcomm/eld/archive/v22.1.0-rc3.tar.gz"
+ELD_SRC_URL="https://github.com/qualcomm/eld/archive/23.1.0-rc1.tar.gz"
 LLVM_TESTS_SRC_URL="https://github.com/llvm/llvm-test-suite/archive/refs/tags/llvmorg-${VER}.tar.gz"
 MUSL_SRC_URL="https://github.com/quic/musl/archive/hexagon-v1.2.4-dec-2025.tar.gz"
 LINUX_SRC_URL="https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.13.5.tar.xz"
